d24bdee8abc5fb69918750841a704a77189245c5
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-11-29 14:57:54 +0700

    menambahkan add services

--- ASPWebFormClient/Services/BarangServices.cs
+++ ASPWebFormClient/Services/BarangServices.cs
@@ -51,6 +51,36 @@ namespace ASPWebFormClient.Services
             return response.Data;
         }
 
+        public void InsertBarang(string kodebarang, string namabarang, int stok,
+            decimal hargabeli, decimal hargajual)
+        {
+            var newBarang = new Barang
+            {
+                kodebarang = kodebarang,
+                namabarang = namabarang,
+                stok = stok,
+                hargabeli = hargabeli,
+                hargajual = hargajual
+            };
+
+            var request = new RestRequest("api/BarangDb", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            //data yg akan dikirimkan ke api
+            request.AddJsonBody(newBarang);
+
+            try
+            {
+                var response = _restClient.Execute(request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    throw new Exception($"Error: gagal menambah data - {response.ErrorMessage}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
     }
 }
