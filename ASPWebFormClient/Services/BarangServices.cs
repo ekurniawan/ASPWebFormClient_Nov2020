5d220f04ac5898d90e85ad129cba04facd016ac5
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-11-29 13:28:23 +0700

    mengakses get menggunakan restsharp

--- ASPWebFormClient/Services/BarangServices.cs
+++ ASPWebFormClient/Services/BarangServices.cs
@@ -2,21 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using ASPWebFormClient.Models;
 using RestSharp;
 
 namespace ASPWebFormClient.Services
 {
     public class BarangServices
     {
-        //
+        //ObjectDataSource
         private RestClient _restClient;
         public BarangServices()
         {
             _restClient = new RestClient
             {
-                BaseUrl = new Uri("https://myapipgsql.azurewebsites.net/api/BarangDb")
+                BaseUrl = new Uri("https://myapipgsql.azurewebsites.net")
             };
+        }
+
+        public IEnumerable<Barang> GetAll()
+        {
+            var request = new RestRequest("api/BarangDb", Method.GET)
+            {
+                RequestFormat = DataFormat.Json
+            };
+
+            var response = _restClient.Execute<List<Barang>>(request);
+            if(response.Data==null)
+            {
+                throw new Exception($"Error: {response.ErrorMessage}");
+            }
+            return response.Data;
         }
     }
 }
